d4a52c087a44ecc358d128b5a18ffb18f64d50ad
Meysam Bavi <meybsky@gmail.com>
2023-06-10 15:29:17 +0330

    Rename test to sample

--- scrape_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-cd ./scraper
-scrapy crawl laptops -a links_file=../datasets/test_links.txt
-python ./jsonl_to_csv.py -i ./data.jsonl -o ../datasets/test_data.csv
-rm ./data.jsonl
